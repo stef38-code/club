39f8cb143854c76d1d805fe4bb6aa1349ed385bd
stephane betton <stphane.betton@ag2rlamondiale.fr>
2022-05-31 10:32:22 +0200

    add ControllerAdvice

--- src/main/java/org/stephane/club/config/advice/GlobalErrorHandler.java
+++ src/main/java/org/stephane/club/config/advice/GlobalErrorHandler.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.stephane.club.share.exception.DataExistsException;
 import org.stephane.club.share.exception.DataNotFoundException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +18,16 @@ public class GlobalErrorHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     String dataNotFoundHandler(HttpServletRequest request,
                                    HttpServletResponse response, Exception ex) {
+
+        // do something with request or response
+
+        return ex.getMessage();
+    }
+    @ResponseBody
+    @ExceptionHandler(DataExistsException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String dataExistsHandler(HttpServletRequest request,
+                               HttpServletResponse response, Exception ex) {
 
         // do something with request or response
 
