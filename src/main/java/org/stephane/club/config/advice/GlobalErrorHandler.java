dbd707718fe1662821aee86d6170f966e4dce089
stephane betton <stphane.betton@ag2rlamondiale.fr>
2022-05-31 10:29:41 +0200

    add ControllerAdvice

---
+++ src/main/java/org/stephane/club/config/advice/GlobalErrorHandler.java
@@ -0,0 +1,25 @@
+package org.stephane.club.config.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.stephane.club.share.exception.DataNotFoundException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@ControllerAdvice
+public class GlobalErrorHandler {
+    @ResponseBody
+    @ExceptionHandler(DataNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String dataNotFoundHandler(HttpServletRequest request,
+                                   HttpServletResponse response, Exception ex) {
+
+        // do something with request or response
+
+        return ex.getMessage();
+    }
+}
